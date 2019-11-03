bcc9c5343baa0c9d4be3053e5ac7c3a6a733c553
Sotnyk <danylos2001@gmail.com>
2019-11-03 15:48:02 +0200

    Some functions in ImmutableArrayList and tests for them added

--- src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
+++ src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
@@ -1,5 +1,115 @@
 package ua.edu.ucu.collections.immutable;
 
-public class ImmutableArrayList {
-    
+import java.util.Arrays;
+
+public class ImmutableArrayList implements ImmutableList {
+
+    private final Object[] elements;
+
+    private void indexCheck(int index) throws IndexOutOfBoundsException {
+        if (index > this.elements.length || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public ImmutableArrayList() {
+        this.elements = new Object[] {};
+    }
+
+    public ImmutableArrayList(Object[] e) {
+        this.elements = e;
+    }
+
+    @Override
+    public ImmutableList add(Object e) {
+        return this.add(this.elements.length, e);
+    }
+
+    @Override
+    public ImmutableList add(int index, Object e) throws IndexOutOfBoundsException {
+        indexCheck(index);
+        Object[] newList = Arrays.copyOf(this.elements, this.elements.length + 1);
+        newList[index] = e;
+        for (int i = index + 1; i < this.elements.length + 1; i++) {
+            newList[i] = this.elements[i-1];
+        }
+        return new ImmutableArrayList(newList);
+    }
+
+    @Override
+    public ImmutableList addAll(Object[] c) {
+        return this.addAll(this.elements.length, c);
+    }
+
+    @Override
+    public ImmutableList addAll(int index, Object[] c) throws IndexOutOfBoundsException {
+        indexCheck(index);
+        Object[] newList = Arrays.copyOf(this.elements, this.elements.length + c.length);
+        for (int i = index; i < c.length + index; i++) {
+            newList[i] = c[i - index];
+        }
+        for (int i = c.length + index; i < c.length + this.elements.length; i++) {
+            newList[i] = this.elements[i - c.length];
+        }
+        return new ImmutableArrayList(newList);
+    }
+
+    @Override
+    public Object get(int index) throws IndexOutOfBoundsException{
+        indexCheck(index);
+        return this.elements[index];
+    }
+
+    @Override
+    public ImmutableList remove(int index) throws IndexOutOfBoundsException{
+        indexCheck(index);
+        Object[] newList = Arrays.copyOf(this.elements, this.elements.length - 1);
+        for (int i = index; i < this.elements.length - 1; i++) {
+            newList[i] = this.elements[i+1];
+        }
+        return new ImmutableArrayList(newList);
+    }
+
+    @Override
+    public ImmutableList set(int index, Object e) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object e) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public ImmutableList clear() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public String toString() {
+        String resString = "";
+        for (int i = 0; i < this.elements.length; i++) {
+
+            resString += String.valueOf(this.elements[i]);
+            if (i != this.elements.length-1) {
+                resString += ", ";
+            }
+        }
+        return resString;
+    }
 }
--- src/test/java/ua/edu/ucu/collections/immutable/ImmutableArrayListTest.java
+++ src/test/java/ua/edu/ucu/collections/immutable/ImmutableArrayListTest.java
@@ -2,11 +2,79 @@ package ua.edu.ucu.collections.immutable;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class ImmutableArrayListTest {
-    
+
     @Test
-    public void testSomeMethod() {
+    public void testAddMethod() {
+        ImmutableArrayList a = new ImmutableArrayList();
+        ImmutableList b = a.add(1);
+        assertEquals(b.toString(), new ImmutableArrayList(new Object[] {1}).toString());
+        assertNotEquals(b.toString(), a.toString());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddWithIndexMethodException() {
+        ImmutableArrayList a = new ImmutableArrayList();
+        ImmutableList b = a.add(3, 1);
+    }
+
+    @Test
+    public void testAddWithIndexMethod() {
+        ImmutableArrayList a = new ImmutableArrayList();
+        ImmutableList b = a.add(0, 1);
+        assertEquals(b.toString(), new ImmutableArrayList(new Object[] {1}).toString());
+        assertNotEquals(b.toString(), a.toString());
+    }
+
+    @Test
+    public void testAddAllMethod() {
+        ImmutableArrayList a = new ImmutableArrayList(new Object[] {1,2,3});
+        ImmutableList b = a.addAll(new Object[] {4,5,6});
+        assertEquals(b.toString(), new ImmutableArrayList(new Object[] {1,2,3,4,5,6}).toString());
+        assertNotEquals(b.toString(), a.toString());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddAllWithIndexMethodException() {
+        ImmutableArrayList a = new ImmutableArrayList(new Object[] {1,2,3});
+        ImmutableList b = a.addAll(5, new Object[] {4,5,6});
+    }
+
+    @Test
+    public void testAddAllWithIndexMethod() {
+        ImmutableArrayList a = new ImmutableArrayList(new Object[] {1,2,3});
+        ImmutableList b = a.addAll(2, new Object[] {4,5,6});
+        assertEquals(b.toString(), new ImmutableArrayList(new Object[] {1,2,4,5,6,3}).toString());
+        assertNotEquals(b.toString(), a.toString());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetMethodException() {
+        ImmutableArrayList a = new ImmutableArrayList(new Object[] {1,2,3});
+        Object elem = a.get(7);
+    }
+
+    @Test
+    public void testGetMethod() {
+        ImmutableArrayList a = new ImmutableArrayList(new Object[] {1,2,3});
+        Object elem = a.get(2);
+        assertEquals(elem, 3);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveMethodException() {
+        ImmutableArrayList a = new ImmutableArrayList(new Object[] {1,2,3});
+        Object elem = a.remove(7);
+    }
+
+    @Test
+    public void testRemoveMethod() {
+        ImmutableArrayList a = new ImmutableArrayList(new Object[] {1,2,3});
+        ImmutableList b = a.remove(1);
+        assertEquals(b.toString(), new ImmutableArrayList(new Object[] {1,3}).toString());
+        assertNotEquals(b.toString(), a.toString());
     }
     
 }
